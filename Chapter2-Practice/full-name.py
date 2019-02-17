3c8c1ef62f3e8205fc3d8fd75f12ba80478f6c17
Beautyi <1210112866@qq.com>
2019-02-17 22:40:31 +0800

    full_name

---
+++ Chapter2-Practice/full-name.py
@@ -0,0 +1,10 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name+" "+last_name
+print(full_name)
+print("Hello,"+full_name.title()+"!")
+
+a = "ada"
+b = "lovelace"
+c = a+" "+b
+print(c)
