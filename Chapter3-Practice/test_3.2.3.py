afc927e4cca70ac45f24bca883429161477bed50
Beautyi <1210112866@qq.com>
2019-02-19 15:31:07 +0800

    1

--- Chapter3-Practice/test_3.2.3.py
+++ Chapter3-Practice/test_3.2.3.py
@@ -1,4 +1,8 @@
 names = ['Jobs', 'Mia', 'Ava', 'Zoe']
 print(names)
-del names[0]
-print(names)
+del names[0]#永久删除任意已知位置元素del加空格加变量名[索引]
+print(names)#['Mia', 'Ava', 'Zoe']
+names.pop()#删除最后一个元素，删除后仍可使用
+print(names)#['Mia', 'Ava']
+
+
