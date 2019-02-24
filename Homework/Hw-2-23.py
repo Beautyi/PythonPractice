afd200d67884ceabd8b39ac5a6bd83d2bcd8cd22
hysteriai <zhemeaisunday@gmail.com>
2019-02-24 16:34:20 +0800

    Create Hw-2-23.py

---
+++ Homework/Hw-2-23.py
@@ -0,0 +1,21 @@
+'''
+获取用户输入的字符串然后按如下形式输出。用list实现。
++------------+
+|            |
+| User input |
+|            |
++------------+
+'''
+
+user_input = input("Please input some words:\n")
+input_len = len(user_input)
+
+first = "+" + input_len * "-" + "+"
+second = "|" + input_len * " " + "|"
+usr_input = "|" + user_input + "|"
+
+print(first)
+print(second)
+print(usr_input)
+print(second)
+print(first)
